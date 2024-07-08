2b5b6f5fed3c1a56a7079e26adc622f10e14842b
om salunke <salunkeom474@gmail.com>
2024-07-08 06:07:13 -0700

    db code updated

--- app/db/db.js
+++ app/db/db.js
@@ -4,6 +4,10 @@ const connectDB = async () => {
     if (mongoose.connection.readyState >= 1) {
       return;
     }
-    return mongoose.connect(`${process.env.DB_URL}`);
+    return   await mongoose.connect(`${process.env.DB_URL}?retryWrites=true&w=majority&appName=CertificateGenerator`, {
+      dbName: "chai",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+  })
   };
   export default connectDB;
